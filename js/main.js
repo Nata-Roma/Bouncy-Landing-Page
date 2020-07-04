194c2eb703cc8bf52c7b3ac132e9eeecd39fd861
Nata-Roma <sharkell@mail.ru>
2020-07-04 17:51:32 +0300

    final

---
+++ js/main.js
@@ -0,0 +1,18 @@
+$(document).ready(function () {
+    
+    $('.slider.slick-horisontal').slick({
+        arrows: false,
+        dots: true
+    });
+    
+    $('.slider.slick-vertical').slick({
+        arrows: false,
+        vertical: true,
+        verticalSwiping: true,
+        slidesToShow: 1,
+        dots: true
+    });
+
+    
+
+});
